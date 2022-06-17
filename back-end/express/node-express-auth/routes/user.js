1c9c910ab193535e50d2bdaac63af10fef34eaa5
Akash Das <mfs.akash@gmail.com>
2022-06-18 00:30:30 +0530

    added user login route

--- back-end/express/node-express-auth/routes/user.js
+++ back-end/express/node-express-auth/routes/user.js
@@ -78,4 +78,64 @@ router.post(
   }
 );
 
+router.post("/login", [
+  check("email", "Please enter a valid email").isEmail(),
+  check("password", "Please enter a valid password").isLength({
+    min: 6,
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({
+        email,
+      });
+      if (!user)
+        return res.status(400).json({
+          message: "User Does Not Exist!",
+        });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          message: "Incorrect Password!",
+        });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token,
+          });
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error!",
+      });
+    }
+  },
+]);
+
 module.exports = router;

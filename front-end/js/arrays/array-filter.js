e5b8802ee6c61007376dcda0c514f68964c811b9
Akash Das <mfs.akash@gmail.com>
2022-07-26 16:56:03 +0530

    added js array map(), filter(), reduce() demo

---
+++ front-end/js/arrays/array-filter.js
@@ -0,0 +1,68 @@
+/**
+ * Example-1
+ */
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function to check even numbers
+function checkEven(number) {
+  if (number % 2 == 0) return true;
+  else return false;
+}
+
+// create a new array by filter even numbers from the numbers array
+let evenNumbers = numbers.filter(checkEven);
+console.log(evenNumbers);
+
+/**
+ * Example-2
+ * Filtering out values from an Array
+ */
+
+const prices = [1800, 2000, null, 3000, 5000, "Thousand", 500, 8000];
+
+function checkPrice(element) {
+  return element > 2000 && !Number.isNaN(element);
+}
+
+let filteredPrices = prices.filter(checkPrice);
+console.log(filteredPrices); // [ 3000, 5000, 8000 ]
+
+// using arrow function
+let newPrices = prices.filter((price) => price > 2000 && !Number.isNaN(price));
+console.log(newPrices); // [ 3000, 5000, 8000 ]
+
+/**
+ * Example-3
+ * Searching an Array
+ */
+
+const languages = [
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "C",
+  "C++",
+  "Swift",
+  "PHP",
+  "Java",
+];
+
+function searchFor(arr, query) {
+  function condition(element) {
+    return element.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  }
+  return arr.filter(condition);
+}
+
+let newArr = searchFor(languages, "ja");
+console.log(newArr); // [ 'JavaScript', 'Java' ]
+
+// using arrow function
+const searchArr = (arr, query) =>
+  arr.filter(
+    (element) => element.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+let newLanguages = searchArr(languages, "p");
+console.log(newLanguages); // [ 'JavaScript', 'Python', 'PHP' ]
---
+++ front-end/js/arrays/array-map.js
@@ -0,0 +1,60 @@
+/**
+ * Example-1
+ */
+
+let numbers = [2, 4, 6, 8, 10];
+
+// function to return the square of a number
+function square(number) {
+  return number * number;
+}
+
+// apply square() function to each item of the numbers list
+let square_numbers = numbers.map(square);
+console.log(square_numbers);
+
+/**
+ * Example-2
+ * Mapping array elements using custom function
+ */
+
+const prices = [1800, 2000, 3000, 5000, 500, 8000];
+
+let newPrices = prices.map(Math.sqrt);
+// [ 42.42640687119285, 44.721359549995796, 54.772255750516614,
+//   70.71067811865476, 22.360679774997898, 89.44271909999159 ]
+console.log(newPrices);
+
+// custom arrow function
+const string = "JavaScript";
+const stringArr = string.split(""); // array with individual string character
+
+let asciiArr = stringArr.map((x) => x.charCodeAt(0));
+
+// map() does not change the original array
+console.log(stringArr); // ['J', 'a', 'v', 'a','S', 'c', 'r', 'i', 'p', 't']
+
+console.log(asciiArr); // [ 74,  97, 118,  97, 83,  99, 114, 105, 112, 116 ]
+
+/**
+ * Example-3
+ * map() for object elements in array
+ */
+
+const employees = [
+  { name: "Adam", salary: 5000, bonus: 500, tax: 1000 },
+  { name: "Noah", salary: 8000, bonus: 1500, tax: 2500 },
+  { name: "Fabiano", salary: 1500, bonus: 500, tax: 200 },
+  { name: "Alireza", salary: 4500, bonus: 1000, tax: 900 },
+];
+
+// calculate the net amount to be given to the employees
+const calcAmt = (obj) => {
+  newObj = {};
+  newObj.name = obj.name;
+  newObj.netEarning = obj.salary + obj.bonus - obj.tax;
+  return newObj;
+};
+
+let newArr = employees.map(calcAmt);
+console.log(newArr);

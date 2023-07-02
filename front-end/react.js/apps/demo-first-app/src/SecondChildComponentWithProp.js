9fb310ea515f22cd443a5899a3177f1077990aa1
Akash Das <mfs.akash@gmail.com>
2023-07-02 19:47:06 +0530

    added new react app built from scratch

--- front-end/react.js/apps/demo-first-app/src/SecondChildComponentWithProp.js
+++ front-end/react.js/apps/demo-first-app/src/SecondChildComponentWithProp.js
@@ -1,9 +1,11 @@
 function SecondChildComponentWithProp({ myProp }) {
+    let messageEnding = "!!!";
+
     return (
         <>
             <h2>Second Child Component With Prop</h2>
             <p>
-                Prop Data: {myProp}
+                Prop Data: {myProp}{messageEnding}
             </p>
         </>
     );
